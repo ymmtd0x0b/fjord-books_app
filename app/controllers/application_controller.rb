7f1981f12f189db33745a11ebc77bd9d79a01665
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-04 13:13:45 +0900

    Rubocopの指摘を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :postal_code, :prefecture, :address_city, :address_street, :address_building, :self_introduction])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :postal_code, :prefecture, :address_city, :address_street, :address_building, :self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[name email postal_code prefecture address_city address_street address_building self_introduction])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name email postal_code prefecture address_city address_street address_building self_introduction])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
+class UsersController < ApplicationController
   def index
     @users = User.order(:id).page(params[:page])
   end
@@ -7,5 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
 end
