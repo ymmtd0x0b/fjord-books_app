6404530fc848073ac20c81e5da4eff9158bb86c3
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-04 10:18:06 +0900

    ユーザーの作成/パスワード再設定をメールで行う機能を実装(開発のみ:letter_opener_webの導入)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users , only: [:index, :show]
+
+  # Development
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
