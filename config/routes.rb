5a02877e2bc9547292a2af5abdd191b6b14b5638
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-07 20:26:50 +0900

    URIの Sign in / Sign out を Log in / Log out に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'books#index'
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'log_in', sign_out: 'log_out' }
   resources :books
   resources :users , only: [:index, :show]
 
