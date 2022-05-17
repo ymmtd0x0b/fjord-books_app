c0acc9c46c63799f09c02c1b46a50669db8e69b4
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-17 11:23:32 +0900

    usersテーブルのマイグレーションファイルを統合/データベースのリセット

--- db/migrate/20220503232432_add_address_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddAddressToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :prefecture, :string
-    add_column :users, :city, :string
-    add_column :users, :street, :string
-    add_column :users, :building, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_03_232432) do
+ActiveRecord::Schema.define(version: 2022_05_03_200949) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2022_05_03_232432) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", null: false
     t.string "postal_code"
     t.text "self_introduction"
     t.string "prefecture"
