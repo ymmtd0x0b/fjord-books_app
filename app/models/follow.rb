7c86154e2dee4a610cab502ae2cf012b25665ce2
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-19 13:09:33 +0900

    UserモデルとFollowモデルのアソシエーションの設定

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,6 @@
 class Follow < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
+  validates :follower_id, presence: true, uniqueness: { scope: :followed_id }
+  validates :followed_id, presence: true, uniqueness: { scope: :follower_id }
 end
