43d2f5a1c21cc98bb240a686c492f95e13fce856
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-26 14:22:48 +0900

    i18nの定義と呼び出し部分の一部を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @title = :followings
+    @title = t('views.follow.followings')
     @users = User.find(params[:id]).followings.with_attached_avatar.order(:id).page(params[:page])
     render 'followings_and_followers_list'
   end
 
   def followers
-    @title = :followers
+    @title = t('views.follow.followers')
     @users = User.find(params[:id]).followers.with_attached_avatar.order(:id).page(params[:page])
     render 'followings_and_followers_list'
   end
