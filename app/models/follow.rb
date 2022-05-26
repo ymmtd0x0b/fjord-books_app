87252b4bf9cc1219d391debc9797b5b3775d448c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-26 14:06:08 +0900

    同じ役割を持ったバリデーションを削除

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,6 +3,5 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
-  validates :follower_id, presence: true, uniqueness: { scope: :followed_id }
-  validates :followed_id, presence: true, uniqueness: { scope: :follower_id }
+  validates :follower_id, uniqueness: { scope: :followed_id }
 end
