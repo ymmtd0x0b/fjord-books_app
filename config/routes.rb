315da09bb0f7bde9efea9dbb84ff9b493a840784
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-05-29 21:16:00 +0900

    ルーティングの書き方を修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: %i[create destroy], controller: 'books/comments'
+    resources :comments, only: %i[create destroy], module: :books
   end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
     end
   end
   resources :reports do
-    resources :comments, only: %i[create destroy], controller: 'reports/comments'
+    resources :comments, only: %i[create destroy], module: :reports
   end
 end
