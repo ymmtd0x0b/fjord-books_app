07c8399582c15d5fdb90befdc0796c9af801a326
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-06-03 07:05:06 +0900

    Reportモデルのテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @report = reports(:report_1)
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#created_on' do
+    assert_not_equal DateTime.now.next_year.to_date, @report.created_on
+    assert_equal DateTime.now.to_date, @report.created_on
+  end
+
+  test '#editable?' do
+    assert_not @report.editable?(@bob)
+    assert @report.editable?(@alice)
+  end
 end
