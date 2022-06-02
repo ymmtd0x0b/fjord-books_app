edc6c2f02d5678070f201ed242e25f8820c21f18
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-06-02 23:12:56 +0900

    未使用の自動生成ファイルを削除

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -1,47 +1,47 @@
 # frozen_string_literal: true
 
-require 'application_system_test_case'
+# require 'application_system_test_case'
 
-class BooksTest < ApplicationSystemTestCase
-  setup do
-    @book = books(:one)
-  end
+# class BooksTest < ApplicationSystemTestCase
+#   setup do
+#     @book = books(:one)
+#   end
 
-  test 'visiting the index' do
-    visit books_url
-    assert_selector 'h1', text: 'Books'
-  end
+#   test 'visiting the index' do
+#     visit books_url
+#     assert_selector 'h1', text: 'Books'
+#   end
 
-  test 'creating a Book' do
-    visit books_url
-    click_on 'New Book'
+#   test 'creating a Book' do
+#     visit books_url
+#     click_on 'New Book'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+#     fill_in 'Memo', with: @book.memo
+#     fill_in 'Title', with: @book.title
+#     click_on 'Create Book'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
-  end
+#     assert_text 'Book was successfully created'
+#     click_on 'Back'
+#   end
 
-  test 'updating a Book' do
-    visit books_url
-    click_on 'Edit', match: :first
+#   test 'updating a Book' do
+#     visit books_url
+#     click_on 'Edit', match: :first
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+#     fill_in 'Memo', with: @book.memo
+#     fill_in 'Title', with: @book.title
+#     click_on 'Update Book'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
-  end
+#     assert_text 'Book was successfully updated'
+#     click_on 'Back'
+#   end
 
-  test 'destroying a Book' do
-    visit books_url
-    page.accept_confirm do
-      click_on 'Destroy', match: :first
-    end
+#   test 'destroying a Book' do
+#     visit books_url
+#     page.accept_confirm do
+#       click_on 'Destroy', match: :first
+#     end
 
-    assert_text 'Book was successfully destroyed'
-  end
-end
+#     assert_text 'Book was successfully destroyed'
+#   end
+# end
